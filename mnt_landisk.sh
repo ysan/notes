42310939fa46c0d94460dd77a9bb91c3d2dbbf86
Yoshitake Kurihara <kurihara0-lj@infoseek.jp>
2018-10-14 01:28:24 +0900

    mod - mnt_landisk.sh

--- mnt_landisk.sh
+++ mnt_landisk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #sudo mount -t cifs -o sec=ntlm //192.168.0.2/disk /mnt/Landisk
-sudo mount -t cifs -o sec=ntlm -o username=admin,password= //192.168.50.8/disk /mnt/Landisk
+#sudo mount -t cifs -o sec=ntlm -o username=admin,password= //192.168.50.8/disk /mnt/Landisk
+sudo mount -t cifs -o sec=ntlm,vers=1.0 -o username=admin,password= //192.168.50.7/disk /mnt/Landisk
