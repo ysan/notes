4f0db1bb3d8f09c2e4d314064cd65a1aa4b367f5
kurihara <kurihara0-lj@infoseek.jp>
2019-04-23 13:37:40 +0900

    fix typo testcereal/setup.sh

--- testcereal/setup.sh
+++ testcereal/setup.sh
@@ -10,7 +10,7 @@ fi
 if [ ! -d "./cereal" ]; then
 	git clone https://github.com/USCiLab/cereal
 	if [ ! ${RET} -eq 0 ]; then
-		echo "git clone failure. [git clone https://github.com/msgpack/msgpack-c]"
+		echo "git clone failure. [git clone https://github.com/USCiLab/cereal]"
 		exit 1
 	fi
 fi
