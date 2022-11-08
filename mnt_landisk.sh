afd41b04eea0ce06d7ae556d9c89abbbef87a396
yoshi@fedora29 <kurihara0-lj@infoseek.jp>
2022-11-08 12:58:37 +0900

    Update - etc

--- mnt_landisk.sh
+++ mnt_landisk.sh
@@ -2,4 +2,5 @@
 
 #sudo mount -t cifs -o sec=ntlm //192.168.0.2/disk /mnt/Landisk
 #sudo mount -t cifs -o sec=ntlm -o username=admin,password= //192.168.50.8/disk /mnt/Landisk
-sudo mount -t cifs -o sec=ntlm,vers=1.0 -o username=admin,password= //192.168.50.7/disk /mnt/Landisk
+#sudo mount -t cifs -o sec=ntlm,vers=1.0 -o username=admin,password= //192.168.50.7/disk /mnt/Landisk
+sudo mount -t cifs -o sec=ntlm,vers=1.0 -o username=admin,password=admin //LANDISK-C35758.local/disk1 /mnt/Landisk
