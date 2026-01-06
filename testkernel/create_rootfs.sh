a6eb03c2b8c5cc5447c769fe0495ecbebfb74e88
Yoshitake Kurihara <kurihara0-lj@infoseek.jp>
2026-01-07 00:16:01 +0900

    Update - create_rootfs.sh

--- testkernel/create_rootfs.sh
+++ testkernel/create_rootfs.sh
@@ -5,49 +5,58 @@ if [ ! "$(id -u)" -eq 0  ]; then
 	exit 1
 fi
 
-PREBUILT_ROOTFS=${HOME}/prog/github_repos/busybox/_install
-TARGET_DIR=./rootfs
-TARGET_NAME=rootfs.img
+WORK_DIR="./rootfs"
+TARGET_IMAGE="./rootfs.img"
 
-if [ ! -d "${PREBUILT_ROOTFS}" ]; then
-	echo "Not found prebuilt rootfs directory... [${PREBUILT_ROOTFS}]"
+if [ -z "${PREBUILTS_DIR}" ]; then
+	echo "Please set PREBUILTS_DIR."
+	echo "  ex. export PREBUILTS_DIR=\"path/to\" && $0"
+	echo "  ex. PREBUILTS_DIR=\"path/to\" $0"
+	exit 1
+fi
+if [ ! -d "${PREBUILTS_DIR}" ]; then
+	echo "Not found prebuilt rootfs directory... [${PREBUILTS_DIR}]"
 	exit 1
 fi
 
-REAL_PATH=$(realpath ${TARGET_DIR})
+echo "PREBUILTS_DIR: ${PREBUILTS_DIR}"
+echo "WORK_DIR: ${WORK_DIR}"
+echo "TARGET_IMAGE: ${TARGET_IMAGE}"
+
+REAL_PATH=$(realpath ${WORK_DIR})
 if [ "${REAL_PATH}" == "/" ]; then
 	echo "Please use a target directory other than /."
 	exit 1
 fi
-if [ -d "${TARGET_DIR}" ]; then
-	echo "rm ${TARGET_DIR}"
-	rm -rf ${TARGET_DIR}
-	mkdir -p ${TARGET_DIR}
+if [ -d "${WORK_DIR}" ]; then
+	echo "rm ${WORK_DIR}"
+	rm -rf ${WORK_DIR}
+	mkdir -p ${WORK_DIR}
 fi
 
-REAL_PATH=$(realpath ${TARGET_NAME})
+REAL_PATH=$(realpath ${TARGET_IMAGE})
 if [ "${REAL_PATH}" == "/" ]; then
 	echo "Please use a target name other than /."
 	exit 1
 fi
-if [ -e "${TARGET_NAME}" ]; then
-	echo "rm ${TARGET_NAME}"
-	rm -f ${TARGET_NAME}
+if [ -e "${TARGET_IMAGE}" ]; then
+	echo "rm ${TARGET_IMAGE}"
+	rm -f ${TARGET_IMAGE}
 fi
 
 set -e
 
-mkdir -p ${TARGET_DIR}/{proc,dev,sys,run,etc/init.d,mnt}
-cp -pr "${PREBUILT_ROOTFS}"/* ${TARGET_DIR}
-mknod ${TARGET_DIR}/dev/console c 5 1
-mknod ${TARGET_DIR}/dev/null c 1 3
-mknod ${TARGET_DIR}/dev/ram b 1 0
-mknod ${TARGET_DIR}/dev/zero c 1 5
-mknod ${TARGET_DIR}/dev/urandom c 1 9
-mknod ${TARGET_DIR}/dev/ttyS0 c 4 64
+mkdir -p ${WORK_DIR}/{proc,dev,sys,run,etc/init.d,mnt}
+cp -pr "${PREBUILTS_DIR}"/* ${WORK_DIR}
+mknod ${WORK_DIR}/dev/console c 5 1
+mknod ${WORK_DIR}/dev/null c 1 3
+mknod ${WORK_DIR}/dev/ram b 1 0
+mknod ${WORK_DIR}/dev/zero c 1 5
+mknod ${WORK_DIR}/dev/urandom c 1 9
+mknod ${WORK_DIR}/dev/ttyS0 c 4 64
 echo "append directories -- done"
 
-cat <<EOF > ${TARGET_DIR}/etc/init.d/rcS
+cat <<EOF > ${WORK_DIR}/etc/init.d/rcS
 #!/bin/sh
 mount -t proc none /proc
 mount -t sysfs none /sys
@@ -60,11 +69,11 @@ mount -t cgroup -o cpu cgroup /sys/fs/cgroup/cpu
 mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
 /sbin/mdev -s
 EOF
-chmod 755 ${TARGET_DIR}/etc/init.d/rcS
-echo "create ${TARGET_DIR}/etc/init.d/rcS -- done"
+chmod 755 ${WORK_DIR}/etc/init.d/rcS
+echo "create ${WORK_DIR}/etc/init.d/rcS -- done"
 
-cd ${TARGET_DIR}
-find . | cpio -o -H newc | gzip > ../${TARGET_NAME}
+cd ${WORK_DIR}
+find . | cpio -o -H newc | gzip > ../${TARGET_IMAGE}
 echo "archive rootfs -- done"
 
 echo -e "\ncompleted\n"
